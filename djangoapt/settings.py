7b2c1849fd08224c5d316164bcabc639ebf70e36
javawork <javawork93@gmail.com>
2020-11-10 21:40:10 +0900

    Remove key

--- djangoapt/settings.py
+++ djangoapt/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'ucnb@in0dq@_&1+e&t-q64gdfpm^3_e-v*ue3vh%(95jld^vg&'
+SECRET_KEY = 'YOUR_SECRET_KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
